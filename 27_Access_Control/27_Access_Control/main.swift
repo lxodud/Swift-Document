2fbd9b7e85c2057d92d07da4ced03046a69389c8
lxodud <xodud1998223@naver.com>
2022-09-06 17:49:06 +0900

    27-Access Control

--- 27_Access_Control/27_Access_Control/main.swift
+++ 27_Access_Control/27_Access_Control/main.swift
@@ -2,10 +2,48 @@
 //  main.swift
 //  27_Access_Control
 //
-//  Created by Dylan_Yoon on 2022/06/20.
+//  Created by Hamo on 2022/09/05.
 //
 
-import Foundation
+//public class SomePublicClass {}
+//internal class SomeInternalClass {}
+//fileprivate class SomeFilePrivateClass {}
+//private class SomePrivateClass {}
+//
+//public var somePublicVariable = 0
+//internal let someInternalConstant = 0
+//fileprivate func someFilePrivateFunction() {}
+//private func somePrivateFunction() {}
 
-print("Hello, World!")
+public class SomePublicClass {                  // explicitly public class
+    public var somePublicProperty = 0            // explicitly public class member
+    var someInternalProperty = 0                 // implicitly internal class member
+    fileprivate func someFilePrivateMethod() {}  // explicitly file-private class member
+    private func somePrivateMethod() {}          // explicitly private class member
+}
 
+class SomeInternalClass {                       // implicitly internal class
+    var someInternalProperty = 0                 // implicitly internal class member
+    fileprivate func someFilePrivateMethod() {}  // explicitly file-private class member
+    private func somePrivateMethod() {}          // explicitly private class member
+}
+
+fileprivate class SomeFilePrivateClass {        // explicitly file-private class
+    func someFilePrivateMethod() {}              // implicitly file-private class member
+    private func somePrivateMethod() {}          // explicitly private class member
+}
+
+private class SomePrivateClass {                // explicitly private class
+    func somePrivateMethod() {}                  // implicitly private class member
+}
+
+//func someFunction() -> (SomeInternalClass, SomePrivateClass) {
+//    // function implementation goes here
+//}
+
+public enum CompassPoint {
+    case north
+    case south
+    case east
+    case west
+}
