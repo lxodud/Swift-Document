ac15e5a068314481c3966684c8a1cd5ed51143fd
lxodud <xodud1998223@naver.com>
2022-09-07 21:19:08 +0900

    27-Access Control

--- 27_Access_Control/27_Access_Control/main.swift
+++ 27_Access_Control/27_Access_Control/main.swift
@@ -47,3 +47,119 @@ public enum CompassPoint {
     case east
     case west
 }
+
+// MARK: 함수 접근 수준
+
+//class PrivateClass {
+//    let a: Int = 1
+//}
+
+//func a(privateClass: PrivateClass) {
+//    print(privateClass.a)
+//} // 에러남
+
+//func a() -> PrivateClass? {
+//    return nil
+//} // 에러남
+
+//fileprivate func a(privateClass: PrivateClass) {
+//    print(privateClass.a)
+//}
+
+// MARK: 하위 클래스 상위 클래스
+
+//private class PrivateClass {
+//    let a: Int = 1
+//}
+//
+//private class childClass: PrivateClass {
+//
+//} // Class cannot be declared internal because its superclass is private
+
+// MARK: 재정의
+
+//public class PrivateClass {
+//    let a: Int = 1
+//    fileprivate func someMethod() { }
+//}
+//
+//internal class childClass: PrivateClass {
+//
+//    override internal func someMethod() {
+//        print("abc")
+//    }
+//}
+
+// MARK: 상수, 변수, 프로퍼티
+
+//fileprivate class FilePrivateClass {
+//    let a: Int = 0
+//}
+//
+//private class PrivateClass {
+//    let a: Int = 0
+//}
+
+//internal class InternalClass {
+//    let a: Int = 0
+//}
+//
+//private let some = PrivateClass()
+// -> 에러
+//fileprivate let some = PrivateClass()
+////let some = PrivateClass() -> 에러
+
+// MARK: private(set)
+
+//class SomeClass {
+//    private(set) var a: Int = 0
+//}
+//
+//let number = SomeClass()
+//
+//print(number.a)
+//
+//number.a = 1
+//
+//private let some = PrivateClass()
+//
+//fileprivate let some = FilePrivateClass()
+
+// MARK: fileprivate vs private
+//private class FilePrivateClass {
+//    let a: Int = 2
+//}
+////
+//private class PrivateClass {
+//    public let a = FilePrivateClass()
+//}
+//
+//internal class InternalClass {
+//    let a: Int = 0
+//}
+
+//private let some = PrivateClass()
+// 이건 가능하죠?
+// 가능
+
+//fileprivate let some = PrivateClass()
+//print(some.a.a)
+// 이거도 가능해요
+// 더 높은 접근수준인데 왜 될까요??
+// 가능
+
+//internal let some = PrivateClass()
+// 불가능 인터널임
+
+//fileprivate let some = FilePrivateClass()
+
+//class SomeClass {
+//    private(set) var a: Int = 0
+//}
+//
+//let number = SomeClass()
+//
+//print(number.a)
+
+//number.a = 1
+
