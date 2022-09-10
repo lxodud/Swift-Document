241160ead3b143fcd129570e9cc9438c7d3e2acd
lxodud <xodud1998223@naver.com>
2022-09-11 02:03:03 +0900

    14-Initialization

--- 14_Initalization/14_Initalization/main.swift
+++ 14_Initalization/14_Initalization/main.swift
@@ -6,112 +6,170 @@
 //
 
 // MARK: Setting Initial Values for Stored Properties
-struct Fahrenheit {
-    var temperature: Double
-    init() {
-        temperature = 32.0
-    }
-}
+//struct Fahrenheit {
+//    var temperature: Double
+//    init() {
+//        temperature = 32.0
+//    }
+//}
+//
+//var f = Fahrenheit()
+//print("기본 온도: \(f.temperature)")
+//// 기본 온도: 32.0
+//
+//// MARK: Initialization Parameters
+//struct Celsius {
+//    var temperatureInCelsius: Double
+//
+//    init(fromFahrenheit fahrenheit: Double) {
+//        temperatureInCelsius = (fahrenheit - 32.0) / 1.8
+//    }
+//
+//    init(fromKelvin kelvin: Double) {
+//        temperatureInCelsius = kelvin - 273.15
+//    }
+//}
+//
+//let boilingPointOfWater = Celsius(fromFahrenheit: 212.0)
+//// boilingPointOfWater.temperatureInCelsius == 100.0
+//let freezingPointOfWater = Celsius(fromKelvin: 273.15)
+//// freezingPointOfWater.temperatureInCelsius == 0.0
+//
+//// MARK: Parameter Names and Argument Labels
+//struct Color {
+//    let red: Double
+//    let green: Double
+//    let blue: Double
+//
+//    init(red: Double, green: Double, blue: Double) {
+//        self.red = red
+//        self.green = green
+//        self.blue = blue
+//    }
+//
+//    init(white: Double) {
+//        red = white
+//        green = white
+//        blue = white
+//    }
+//}
+//
+//let magenta = Color(red: 1.0, green: 0.0, blue: 1.0)
+//let halfGray = Color(white: 0.5)
+//
+//// MARK: Initializer Parameters Without Argument Labels
+//extension Celsius {
+//    init(_ celsius: Double) {
+//        temperatureInCelsius = celsius
+//    }
+//}
+//
+//let bodyTemperature = Celsius(37.0)
+//print(bodyTemperature)
+//
+//// MARK: Default Initializers
+//class ShoppingListItem {
+//    var name: String?
+//    var quantity = 1
+//    var purchased = false
+//}
+//
+//var item = ShoppingListItem()
+//
+//// MARK: Memberwise Initializers for Structure Types
+//struct Size {
+//    var width = 0.0
+//    var height = 0.0
+//}
+////let twoByTwo = Size(width: 2.0, height: 2.0)
+////let zeroByTwo = Size(height: 2.0)
+////let zeroByZero = Size()
+//
+//struct Point {
+//    var x = 0.0
+//    var y = 0.0
+//}
+//
+//struct Rect {
+//    var origin = Point()
+//    var size = Size()
+//
+//    init() {}
+//
+//    init(origin: Point, size: Size) {
+//        self.origin = origin
+//        self.size = size
+//    }
+//
+//    init(center: Point, size: Size) {
+//        let originX = center.x - (size.width / 2)
+//        let originY = center.y - (size.height / 2)
+//        self.init(origin: Point(x: originX, y: originY), size: size)
+//    }
+//}
+//
+//let basicRect = Rect()
+//
+//let originRect = Rect(origin: Point(x: 2.0, y: 2.0), size: Size(width: 5.0, height: 5.0))
+//
+//let centerRect = Rect(center: Point(x: 4.0, y: 4.0), size: Size(width: 3.0, height: 3.0))
+//
+//class Vehicle {
+//    var numberOfWheels = 0
+//    var description: String {
+//        return "\(numberOfWheels) wheel(s)"
+//    }
+//}
+//
+//class Bicycle: Vehicle {
+//    override init() {
+//        super.init()
+//        numberOfWheels = 2
+//    }
+//}
+//
+//let bicycle = Bicycle()
+//print(bicycle.description)
+//
+//class Hoverboard: Vehicle {
+//    var color: String
+//
+//    init(color: String) {
+//        self.color = color
+//        // super.init()이 암시적으로 호출된다.
+//    }
+//
+//    override var description: String {
+//        return "\(super.description) in a beautiful \(color)"
+//    }
+//}
+//
+//let hoverboard = Hoverboard(color: "silver")
+//print("Hoverboard: \(hoverboard.description)")
+//// Hoverboard: 0 wheel(s) in a beautiful silver
 
-var f = Fahrenheit()
-print("기본 온도: \(f.temperature)")
-// 기본 온도: 32.0
-
-// MARK: Initialization Parameters
-struct Celsius {
-    var temperatureInCelsius: Double
+class Food {
+    var name: String
     
-    init(fromFahrenheit fahrenheit: Double) {
-        temperatureInCelsius = (fahrenheit - 32.0) / 1.8
+    init(name: String) {
+        self.name = name
     }
     
-    init(fromKelvin kelvin: Double) {
-        temperatureInCelsius = kelvin - 273.15
+    convenience init() {
+        self.init(name: "[Unnamed]")
     }
 }
 
-let boilingPointOfWater = Celsius(fromFahrenheit: 212.0)
-// boilingPointOfWater.temperatureInCelsius == 100.0
-let freezingPointOfWater = Celsius(fromKelvin: 273.15)
-// freezingPointOfWater.temperatureInCelsius == 0.0
+let mysteryMeat = Food()
+let namedMeet = Food(name: "Bacon")
 
-// MARK: Parameter Names and Argument Labels
-struct Color {
-    let red: Double
-    let green: Double
-    let blue: Double
-    
-    init(red: Double, green: Double, blue: Double) {
-        self.red = red
-        self.green = green
-        self.blue = blue
+class RecipeIngredient: Food {
+    var quantity: Int
+    init(name: String, quantity: Int) {
+        self.quantity = quantity
+        super.init(name: name)
     }
-    
-    init(white: Double) {
-        red = white
-        green = white
-        blue = white
+    override convenience init(name: String) {
+        self.init(name: name, quantity: 1)
     }
 }
-
-let magenta = Color(red: 1.0, green: 0.0, blue: 1.0)
-let halfGray = Color(white: 0.5)
-
-// MARK: Initializer Parameters Without Argument Labels
-extension Celsius {
-    init(_ celsius: Double) {
-        temperatureInCelsius = celsius
-    }
-}
-
-let bodyTemperature = Celsius(37.0)
-print(bodyTemperature)
-
-// MARK: Default Initializers
-class ShoppingListItem {
-    var name: String?
-    var quantity = 1
-    var purchased = false
-}
-
-var item = ShoppingListItem()
-
-// MARK: Memberwise Initializers for Structure Types
-struct Size {
-    var width = 0.0
-    var height = 0.0
-}
-//let twoByTwo = Size(width: 2.0, height: 2.0)
-//let zeroByTwo = Size(height: 2.0)
-//let zeroByZero = Size()
-
-struct Point {
-    var x = 0.0
-    var y = 0.0
-}
-
-struct Rect {
-    var origin = Point()
-    var size = Size()
-    
-    init() {}
-    
-    init(origin: Point, size: Size) {
-        self.origin = origin
-        self.size = size
-    }
-    
-    init(center: Point, size: Size) {
-        let originX = center.x - (size.width / 2)
-        let originY = center.y - (size.height / 2)
-        self.init(origin: Point(x: originX, y: originY), size: size)
-    }
-}
-
-let basicRect = Rect()
-
-let originRect = Rect(origin: Point(x: 2.0, y: 2.0), size: Size(width: 5.0, height: 5.0))
-
-let centerRect = Rect(center: Point(x: 4.0, y: 4.0), size: Size(width: 3.0, height: 3.0))
-
-
