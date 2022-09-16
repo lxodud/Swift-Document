988fe167d207f2e5c9429718ee0e887acd2a3709
lxodud <xodud1998223@naver.com>
2022-09-16 16:04:28 +0900

    22_Protocols

--- 22_Protocols/22_Protocols/main.swift
+++ 22_Protocols/22_Protocols/main.swift
@@ -2,10 +2,59 @@
 //  main.swift
 //  22_Protocols
 //
-//  Created by Dylan_Yoon on 2022/06/20.
+//  Created by Hamo on 2022/09/12.
 //
 
-import Foundation
+protocol FullyNamed {
+    var fullName: String { get }
+}
 
-print("Hello, World!")
+struct Person: FullyNamed {
+    var fullName: String
+}
 
+let hamo = Person(fullName: "Hamo Lee")
+
+print(hamo.fullName)
+// Hamo Lee
+
+protocol RandomNumberGenerator {
+    func random() -> Double
+}
+
+class LinearCongruentialGenerator: RandomNumberGenerator {
+    var lastRandom = 42.0
+    let m = 139968.0
+    let a = 3877.0
+    let c = 29573.0
+    func random() -> Double {
+        lastRandom = ((lastRandom * a + c)
+            .truncatingRemainder(dividingBy:m))
+        return lastRandom / m
+    }
+}
+
+let generator = LinearCongruentialGenerator()
+print("Here's a random number: \(generator.random())")
+// Prints "Here's a random number: 0.3746499199817101"
+print("And another one: \(generator.random())")
+// Prints "And another one: 0.729023776863283"
+
+class Dice {
+    let sides: Int
+    let generator: RandomNumberGenerator
+    init(sides: Int, generator: RandomNumberGenerator) {
+        self.sides = sides
+        self.generator = generator
+    }
+    func roll() -> Int {
+        return Int(generator.random() * Double(sides)) + 1
+    }
+}
+
+
+var d6 = Dice(sides: 6, generator: LinearCongruentialGenerator())
+for _ in 1...5 {
+    print("\(generator.random())")
+    print("Random dice roll is \(d6.roll())")
+}
