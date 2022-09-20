0d57cee7d210f794f7d91a42dff5a473a942a3bf
lxodud <xodud1998223@naver.com>
2022-09-21 00:36:35 +0900

    23_Generics

--- 23_Generics/23_Generics/main.swift
+++ 23_Generics/23_Generics/main.swift
@@ -2,10 +2,56 @@
 //  main.swift
 //  23_Generics
 //
-//  Created by Dylan_Yoon on 2022/06/20.
+//  Created by Hamo on 2022/06/20.
 //
 
-import Foundation
+func swapTwoInts(_ a: inout Int, _ b: inout Int) {
+    let temporaryA = a
+    a = b
+    b = temporaryA
+}
 
-print("Hello, World!")
+var someInt = 3
+var anotherInt = 107
+swapTwoInts(&someInt, &anotherInt)
+print("someInt: \(someInt), anotherInt: \(anotherInt)")
+// someInt: 107, anotherInt: 3
 
+func swapTwoValues<T>(_ a: inout T, _ b: inout T) {
+    let temporaryA = a
+    a = b
+    b = temporaryA
+}
+
+//struct IntStack {
+//    var items: [Int] = []
+//    mutating func push(_ item: Int) {
+//        items.append(item)
+//    }
+//    mutating func pop() -> Int {
+//        return items.removeLast()
+//    }
+//}
+
+struct Stack<Element> {
+    var items: [Element] = []
+    
+    mutating func push(_ item: Element) {
+        items.append(item)
+    }
+    
+    mutating func pop() -> Element {
+        return items.removeLast()
+    }
+}
+
+var stringStack = Stack<String>()
+
+stringStack.push("uno")
+stringStack.push("dos")
+stringStack.push("tres")
+stringStack.push("cautro")
+
+let value = stringStack.pop()
+print(value)
+// cautro
