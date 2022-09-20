853548476e081307a4ce4c211af740fed0109c3d
lxodud <xodud1998223@naver.com>
2022-09-20 23:23:44 +0900

    21_Extensions

--- 21_Extenstions/21_Extenstions/main.swift
+++ 21_Extenstions/21_Extenstions/main.swift
@@ -2,10 +2,104 @@
 //  main.swift
 //  21_Extenstions
 //
-//  Created by Dylan_Yoon on 2022/06/20.
+//  Created by Hamo on 2022/06/20.
 //
 
-import Foundation
+struct Size {
+    var width = 0.0
+    var height = 0.0
+}
 
-print("Hello, World!")
+struct Point {
+    var x = 0.0
+    var y = 0.0
+}
 
+struct Rect {
+    var origin = Point()
+    var size = Size()
+}
+
+let defaultRect = Rect()
+let memberwiseRect = Rect(origin: Point(x: 2.0, y: 2.0),
+                          size: Size(width: 5.0, height: 5.0))
+
+extension Rect {
+    init(center: Point, size: Size) {
+        let originX = center.x - (size.width / 2)
+        let originY = center.y - (size.height / 2)
+        self.init(origin: Point(x: originX, y: originY), size: size)
+    }
+}
+
+let centerRect = Rect(center: Point(x: 4.0, y: 4.0),
+                      size: Size(width: 3.0, height: 3.0))
+
+extension Int {
+    func repetitions(task: () -> Void) {
+        for _ in 0..<self {
+            task()
+        }
+    }
+}
+
+4.repetitions {
+    print("오")
+}
+
+extension Int {
+    mutating func square() {
+        self = self * self
+    }
+}
+
+var someInt = 3
+someInt.square()
+print(someInt)
+// 9
+
+extension Int {
+    subscript(digitIndex: Int) -> Int {
+        var decimalBase = 1
+        for _ in 0..<digitIndex {
+            decimalBase *= 10
+        }
+        
+        return (self / decimalBase) % 10
+    }
+}
+
+let number = 7421351235124[0]
+print(number)
+
+extension Int {
+    enum Kind {
+        case negative, zero, positive
+    }
+    var kind: Kind {
+        switch self {
+        case 0:
+            return .zero
+        case let x where x > 0:
+            return .positive
+        default:
+            return .negative
+        }
+    }
+}
+
+func printIntegerKinds(_ numbers: [Int]) {
+    for number in numbers {
+        switch number.kind {
+        case .negative:
+            print("- ", terminator: "")
+        case .zero:
+            print("0 ", terminator: "")
+        case .positive:
+            print("+ ", terminator: "")
+        }
+    }
+    print("")
+}
+printIntegerKinds([3, 19, -27, 0, -6, 0, 7])
+// Prints "+ + - 0 - 0 + "
