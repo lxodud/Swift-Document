1096e1e4ce421052180a292697fca1beddedf6fb
lxodud <xodud1998223@naver.com>
2022-10-04 18:05:24 +0900

    07-Closures

--- 07_Closures/07_Closures/main.swift
+++ 07_Closures/07_Closures/main.swift
@@ -2,10 +2,92 @@
 //  main.swift
 //  07_Closures
 //
-//  Created by Dylan_Yoon on 2022/06/20.
+//  Created by Hamo on 2022/09/25.
 //
 
-import Foundation
+//let names = ["Chris", "Alex", "Ewa", "Barry", "Daniella"]
+//
+//func backward(_ s1: String, _ s2: String) -> Bool {
+//    return s1 > s2
+//}
+//
+//var reversedNames = names.sorted(by: backward)
+//print(reversedNames)
+//// ["Ewa", "Daniella", "Chris", "Barry", "Alex"]
+//
+//reversedNames = names.sorted(by: { (s1: String, s2: String) -> Bool in
+//    return s1 > s2
+//})
+//
+//reversedNames = names.sorted(by: { (s1: String, s2: String) -> Bool in return s1 > s2 } )
+//
+//func trailingClosure(closure: () -> Void) {
+//
+//}
+//
+//trailingClosure(closure: {
+//
+//})
+//
+//trailingClosure() {
+//
+//}
+//
+//trailingClosure {
+//
+//}
+//
+//let digitNames = [
+//    0: "Zero", 1: "One", 2: "Two",   3: "Three", 4: "Four",
+//    5: "Five", 6: "Six", 7: "Seven", 8: "Eight", 9: "Nine"
+//]
+//let numbers = [16, 58, 510]
+//
+//
+//let strings = numbers.map { (number) -> String in
+//    var number = number
+//    var output = ""
+//    repeat {
+//        output = digitNames[number % 10]! + output
+//        number /= 10
+//    } while number > 0
+//    return output
+//}
+//
+//print(strings)
+//// ["OneSix", "FiveEight", "FiveOneZero"]
 
-print("Hello, World!")
+func makeIncrementer(forIncrement amount: Int) -> () -> Int {
+    var runningTotal = 0
+    func incrementer() -> Int {
+        runningTotal += amount
+        return runningTotal
+    }
+    return incrementer
+}
 
+let incremnetByTen = makeIncrementer(forIncrement: 10)
+
+print(incremnetByTen())
+// 10
+print(incremnetByTen())
+// 20
+print(incremnetByTen())
+// 30
+print(incremnetByTen())
+// 40
+
+let incrementBySeven = makeIncrementer(forIncrement: 7)
+
+print(incrementBySeven())
+// 7
+print(incrementBySeven())
+// 14
+print(incrementBySeven())
+// 21
+print(incrementBySeven())
+// 28
+
+
+print(incremnetByTen())
+// 50
